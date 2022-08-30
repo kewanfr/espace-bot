c64193bf3f7811461db8cf17cca192a33afd6638
Kéwan Bernier <mail@kewan.fr>
2022-08-30 16:51:49 +0200

    hotfix(require Logger)

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@
 const { Client, Partials, Collection } = require("discord.js");
 const { Channel, GuildMember, Message, Reaction, ThreadMember, User, GuildScheduledEvent } = Partials;
 
-const Logger = require("./utils/logger");
+const Logger = require("./utils/Logger");
 require("./utils/includes");
 
 console.log("[Discord.js]", "Initializing...");
