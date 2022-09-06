5de18ed6354211314eaf0c60e2afd33dbd0767cf
Kéwan Bernier <mail@kewan.fr>
2022-09-06 19:51:56 +0200

    fix MemberAdd image

--- src/events/discord/member/guildMemberAdd.js
+++ src/events/discord/member/guildMemberAdd.js
@@ -18,35 +18,35 @@ module.exports = {
         client.config.channels.welcome
       );
 
-      fetch(`${client.config.canvasSrvURL}/welcomeimg`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          guildName: member.guild.name,
-          memberPosition: memberPosition,
-          tag: member.user.tag,
-          username: member.user.username,
-          discriminator: member.user.discriminator,
-          avatar: member.user
-            .displayAvatarURL({ format: "png", size: 1024 })
-            .replace(".webp", ".png"),
-        }),
-      }).then(async (response) => {
-        let arrayBuffer = await response.arrayBuffer();
-        let buffer = Buffer.from(arrayBuffer);
+      // fetch(`${client.config.canvasSrvURL}/welcomeimg`, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     guildName: member.guild.name,
+      //     memberPosition: memberPosition,
+      //     tag: member.user.tag,
+      //     username: member.user.username,
+      //     discriminator: member.user.discriminator,
+      //     avatar: member.user
+      //       .displayAvatarURL({ format: "png", size: 1024 })
+      //       .replace(".webp", ".png"),
+      //   }),
+      // }).then(async (response) => {
+      //   let arrayBuffer = await response.arrayBuffer();
+      //   let buffer = Buffer.from(arrayBuffer);
 
+      //   welcomeChannel.send({
+      //     content: `😀 Bienvenue <@${member.user.id}> sur ${member.guild.name} !\nVous êtes le **${memberPosition}**ème membre de ce serveur, on espère qu'il vous plaîra !\n\nPour acceder au serveur, vous devez valider le <#${client.config.channels.rules}>, vous pourrez ensuite vous assigner vos <#${client.config.channels.roles}>`,
+      //     files: [{ attachment: buffer, name: "welcome.png" }],
+      //   });
+      // }).catch((err) => {
         welcomeChannel.send({
-          content: `😀 Bienvenue <@${member.user.id}> sur ${member.guild.name} !\nVous êtes le **${memberPosition}**ème membre de ce serveur, on espère qu'il vous plaîra !\n\nPour acceder au serveur, vous devez valider le <#${client.config.channels.rules}>, vous pourrez ensuite vous assigner vos <#${client.config.channels.roles}>`,
-          files: [{ attachment: buffer, name: "welcome.png" }],
+          content: `😀 Bienvenue <@${member.user.id}> sur ${member.guild.name} !\nVous êtes le **${memberPosition}**ème membre de ce serveur, on espère qu'il vous plaîra !\n\nPour acceder au serveur, vous devez valider le <#${client.config.channels.rules}>, puis vous assigner vos <#${client.config.channels.roles}>`,
         });
-      }).catch((err) => {
-        welcomeChannel.send({
-          content: `😀 Bienvenue <@${member.user.id}> sur ${member.guild.name} !\nVous êtes le **${memberPosition}**ème membre de ce serveur, on espère qu'il vous plaîra !\n\nPour acceder au serveur, vous devez valider le <#${client.config.channels.rules}>, vous pourrez ensuite vous assigner vos <#${client.config.channels.roles}>`,
-        });
-        console.log(err);
-      });
+        // console.log(err);
+      // });
     }
   },
 };
