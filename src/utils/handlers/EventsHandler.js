5c0591fe54f30244d8457932323c49e07b20926a
Kéwan Bernier <mail@kewan.fr>
2022-09-11 00:42:38 +0200

    custom events

--- src/utils/handlers/EventsHandler.js
+++ src/utils/handlers/EventsHandler.js
@@ -22,6 +22,21 @@ module.exports = async (client) => {
 
     log.event(`- ${event.name}`);
   });
+  (await pGlob(`${process.cwd()}/src/events/custom/*/*.js`)).forEach((eventFile) => {
+    const event = require(eventFile);
+
+    if(!event.name) return log.warn(`Evenement non-chargé: pas de nom\nFichier -> ${eventFile}`);
+    
+    if(client.config.mode == "dev" && !event.dev) return ;
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(client, ...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(client, ...args));
+    }
+
+    log.event(`- ${event.name}`);
+  });
 };
 
 const eventList = ["apiResponse", "applicationCommandCreateD", "applicationCommandDeleteD", "applicationCommandUpdateD", "channelCreate", "channelDelete", "channelPinsUpdate", "channelUpdate", "debug", "emojiCreate", "emojiDelete", "emojiUpdate", "error", "guildBanAdd", "guildBanRemove", "guildCreate", "guildDelete", "guildIntegrationsUpdate", "guildMemberAdd", "guildMemberAvailable", "guildMemberRemove", "guildMembersChunk", "guildMemberUpdate", "guildScheduledEventCreate", "guildScheduledEventDelete", "guildScheduledEventUpdate", "guildScheduledEventUserAdd", "guildScheduledEventUserRemove", "guildUnavailable", "guildUpdate", "interactionD", "interactionCreate", "invalidated", "invalidRequestWarning", "inviteCreate", "inviteDelete", "messageD", "messageCreate", "messageDelete", "messageDeleteBulk", "messageReactionAdd", "messageReactionRemove", "messageReactionRemoveAll", "messageReactionRemoveEmoji", "messageUpdate", "presenceUpdate", "rateLimit", "ready", "roleCreate", "roleDelete", "roleUpdate", "shardDisconnect", "shardError", "shardReady", "shardReconnecting", "shardResume", "stageInstanceCreate", "stageInstanceDelete", "stageInstanceUpdate", "stickerCreate", "stickerDelete", "stickerUpdate", "threadCreate", "threadDelete", "threadListSync", "threadMembersUpdate", "threadMemberUpdate", "threadUpdate", "typingStart", "userUpdate", "voiceStateUpdate", "warn", "webhookUpdate"];
