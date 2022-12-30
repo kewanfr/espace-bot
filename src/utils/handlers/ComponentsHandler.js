512d645214f45a6b22d3061650cb129e0b3739e4
Kéwan Bernier <mail@kewan.fr>
2022-12-30 23:42:13 +0100

    refacto

--- src/utils/handlers/ComponentsHandler.js
+++ src/utils/handlers/ComponentsHandler.js
@@ -1,66 +1,80 @@
-const { readdirSync } = require('fs');
+const { readdirSync } = require("fs");
 
 module.exports = async (client) => {
   const { log } = client;
   const componentFolders = readdirSync(`${process.cwd()}/src/components`);
-  for(const folder of componentFolders){
-    const componentFiles = readdirSync(`${process.cwd()}/src/components/${folder}`).filter(file => file.endsWith('.js'));
+  for (const folder of componentFolders) {
+    const componentFiles = readdirSync(
+      `${process.cwd()}/src/components/${folder}`
+    ).filter((file) => file.endsWith(".js"));
 
-    const {buttons, selectMenus, modals} = client;
+    const { buttons, selectMenus, modals } = client;
 
-    switch(folder){
-      case 'buttons':
-        for(const file of componentFiles){
+    switch (folder) {
+      case "buttons":
+        for (const file of componentFiles) {
           const button = require(`${process.cwd()}/src/components/${folder}/${file}`);
-          log.component(`- ${button.data.name}`, "BTN");
-          buttons.set(button.data.name, button);
+          if (client.config.execDev && !button.dev) {
+          } else {
+            log.component(`- ${button.data.name}`, "BTN");
+            buttons.set(button.data.name, button);
+          }
         }
         break;
 
       case "selectMenus":
-        for(const file of componentFiles){
+        for (const file of componentFiles) {
           const menu = require(`${process.cwd()}/src/components/${folder}/${file}`);
-          log.component(`- ${menu.data.name}`, "MENU");
-          selectMenus.set(menu.data.name, menu);
+          if (client.config.execDev && !menu.dev) {
+          } else {
+            log.component(`- ${menu.data.name}`, "MENU");
+            selectMenus.set(menu.data.name, menu);
+          }
         }
         break;
 
       case "modals":
-        for(const file of componentFiles){
+        for (const file of componentFiles) {
           const modal = require(`${process.cwd()}/src/components/${folder}/${file}`);
-          // console.log(`Composant chargé: ${modal.data.name}`);
-          log.component(`- ${modal.data.name}`, "MODAL");
-          modals.set(modal.data.name, modal);
+          if (client.config.execDev && !modal.dev) {
+          } else {
+            // console.log(`Composant chargé: ${modal.data.name}`);
+            log.component(`- ${modal.data.name}`, "MODAL");
+            modals.set(modal.data.name, modal);
+          }
         }
         break;
 
-        case "features":
-          for(const file of componentFiles){
-            const feature = require(`${process.cwd()}/src/components/${folder}/${file}`);
-            if(feature.data.buttons?.length > 0){
-              for(const buttonName of feature.data.buttons){
+      case "features":
+        for (const file of componentFiles) {
+          const feature = require(`${process.cwd()}/src/components/${folder}/${file}`);
+          // console.log(feature);
+          if (client.config.execDev && !feature.dev) {
+          } else {
+            if (feature.data.buttons?.length > 0) {
+              for (const buttonName of feature.data.buttons) {
                 // log.component(`- ${buttonName} (feature: ${feature.data.name})`, "BTN");
                 buttons.set(feature.data.name, feature);
               }
             }
-            if(feature.data.selectMenus?.length > 0){
-              for(const menuName of feature.data.selectMenus){
+            if (feature.data.selectMenus?.length > 0) {
+              for (const menuName of feature.data.selectMenus) {
                 // log.component(`- ${menuName} (feature: ${feature.data.name})`, "MENU");
                 selectMenus.set(feature.data.name, feature);
               }
             }
-            if(feature.data.modals?.length > 0){
-              for(const modalName of feature.data.modals){
+            if (feature.data.modals?.length > 0) {
+              for (const modalName of feature.data.modals) {
                 // log.component(`- ${modalName} (feature: ${feature.data.name})`, "MODAL");
                 modals.set(feature.data.name, feature);
               }
             }
             log.component(`- ${feature.data.name}`, "FEATURE");
           }
-          break;
-        
-      default:
+        }
+        break;
 
+      default:
         break;
     }
   }
